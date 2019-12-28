6e2fc0f9cf0fbbe96dd730404f111f14c67b123b
adroi3 <hedtke36@gmail.com>
2019-12-28 18:21:28 +0100

    Continued

--- FSharpKoans/AboutPipelining.fs
+++ FSharpKoans/AboutPipelining.fs
@@ -27,7 +27,7 @@ module ``about pipelining`` =
         let evens = List.filter isEven numbers
         let result = List.map square evens
 
-        AssertEquality result __
+        AssertEquality result [0; 4; 16]
 
     [<Koan>]
     let SquareEvenNumbersWithParens() =
@@ -39,7 +39,7 @@ module ``about pipelining`` =
 
         let result = List.map square (List.filter isEven numbers)
 
-        AssertEquality result __
+        AssertEquality result 
 
     [<Koan>]
     let SquareEvenNumbersWithPipelineOperator() =
